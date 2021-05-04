768d424a1cdf1e39c673dcaa6359daa7a18d0592
Alonso Brayan N. Mendonça <brayan.neves@gmail.com>
2021-05-04 09:08:05 -0300

    feat: Feita a implementação do método Catalogo.hotelMaisBarato

--- README.md
+++ README.md
@@ -1,14 +1,1 @@
-Nearly empty javascript project with a failing test!
-===
-
-To install the required modules:
-
-```
-$ yarn install
-```
-
-To run the (failing) test:
-
-```
-$ yarn test
-```
+# hotelaria
--- classes/catalogo.class.js
+++ classes/catalogo.class.js
@@ -13,18 +13,35 @@ module.exports = class Catalogo {
   }
   
   add(nome, classificacao, txSemanaReg, txFdsReg, txSemanaFid, txFdsFid) {
-    this.#hoteis.push(new Hotel(nome, classificacao, new Taxa(txSemanaReg, txFdsReg), new Taxa(txSemanaFid, txFdsFid)))
+    const hotel = new Hotel(nome, classificacao, new Taxa(txSemanaReg, txFdsReg), new Taxa(txSemanaFid, txFdsFid))
+    this.#hoteis.push(hotel)
+  }
+
+  formatInput(input) {
+    const arrInput = input.split(': ')
+    const tipoCliente = arrInput[0].toLowerCase()
+
+    const dias = []
+    arrInput[1].split(', ').forEach(dia => {
+      dias.push(new Date(dia))
+    })
+
+    return {tipoCliente: tipoCliente, dias: dias};
   }
 
   hotelMaisBarato(input) {
-    if (input === 'Regular: 16Mar2020(mon), 17Mar2020(tues), 18Mar2020(wed)') {
-      return 'Parque das flores'
-    }
-    if (input === 'Regular: 20Mar2020(fri), 21Mar2020(sat), 22Mar2020(sun)') {
-      return 'Jardim Botânico'
-    }
-    if (input === 'Fidelidade: 26Mar2020(thur), 27Mar2020(fri), 28Mar2020(sat)') {
-      return 'Mar Atlântico'
-    }
+    const queryData = this.formatInput(input)
+
+    const aux = []
+    this.#hoteis.forEach(hotel => {
+      let valorTotal = 0
+      queryData.dias.forEach(dia => {
+        valorTotal += (dia.getDay() === 0 || dia.getDay() === 6) ? hotel.taxaFds[queryData.tipoCliente] : hotel.taxaSemana[queryData.tipoCliente]
+      })
+      aux.push({hotel: hotel.nome, classificacao: hotel.classificacao, valorTotal: valorTotal})
+    })
+
+    const ret = aux.sort((a, b) => { return a.valorTotal-b.valorTotal || b.classificacao-a.classificacao })
+    return ret[0].hotel
   }
 }
