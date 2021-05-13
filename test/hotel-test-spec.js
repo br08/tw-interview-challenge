7112e3082d83fdd7c0db786da637d69d52b4ce5b
Alonso Brayan N. Mendonça <brayan.neves@gmail.com>
2021-05-13 14:51:17 -0300

    refactor: Completa refatoração na padronização do código.

--- test/hotel-test-spec.js
+++ test/hotel-test-spec.js
@@ -1,67 +1,60 @@
-const chai = require('chai')
-const expect = chai.expect
-const Catalogo = require('../classes/catalogo.class')
-const Hotel = require('../classes/hotel.class')
+import { describe, it } from 'mocha'
+import { expect } from 'chai'
 
-describe('Valida o valor da diária de um hotel.', () => {
-  const hotel = new Hotel('Mar Atlântico', 5, 220, 100, 150, 40);
-  const regularSemana = hotel.diaria.valor('regular', 3)
-  const regularFds = hotel.diaria.valor('regular', 0)
-  const fidelidadeSemana = hotel.diaria.valor('fidelidade', 2)
-  const fidelidadeFds = hotel.diaria.valor('fidelidade', 6)
+import Catalogo from '../classes/catalogo.class.mjs'
+import Input from '../classes/input.class.mjs'
+import TabelaPrecos from '../classes/tabela-precos.class.mjs'
+import Hotel from '../classes/hotel.class.mjs'
 
-  it('Retorna valor semana regular', () => {
-    expect(regularSemana).to.equal(220)
+describe('Valida a entrada fornacida pelo usuário.', () => {
+  it('Valida o tipo do cliente corrto.', () => {
+    const input = new Input('Regular: 16Mar2020(mon), 17Mar2020(tues), 18Mar2020(wed)')
+    const { tipoCliente } = input.format()
+    expect(tipoCliente).to.equals('regular')
   })
 
-  it('Retorna valor semana fidelidade', () => {
-    expect(fidelidadeSemana).to.equal(100)
-  })
-
-  it('Retorna valor fds regular', () => {
-    expect(regularFds).to.equal(150)
-  })
-
-  it('Retorna valor fds fidelidade', () => {
-    expect(fidelidadeFds).to.equal(40)
+  it('Valida o array de dias fornecido.', () => {
+    const input = new Input('Regular: 16Mar2020(mon), 17Mar2020(tues), 18Mar2020(wed)')
+    const { dias } = input.format()
+    const diasEsperados = [new Date('16Mar2020(mon)'), new Date('17Mar2020(tues)'), new Date('18Mar2020(wed)')]
+    expect(JSON.stringify(diasEsperados) === JSON.stringify(dias)).to.equals(true)
   })
 })
 
-describe('Valida a entrada fornecida pelo usuário.', () => {
-  const catalogo = new Catalogo()
-  const input = catalogo.formatInput('Regular: 16Mar2020(mon), 17Mar2020(tues), 18Mar2020(wed)')
-  const expectedDays = [new Date('17Mar2020(tues)'), new Date('16Mar2020(mon)'), new Date('18Mar2020(wed)')]
-
-  it('Deve retornar o tipo correto do cliente.', () => {
-    expect(input.tipoCliente).to.equals('regular')
+describe('Valida o valor da diária de um hotel.', () => {
+  const tabela = new TabelaPrecos(100, 90, 80, 70)
+  it('Valida dia semana regular.', () => {
+    expect(tabela.getValorDiaria('regular', 5)).to.equals(100)
   })
 
-  it('Deve retornar o array de datas correto.', () => {
-    expect(JSON.stringify(input.dias.sort()) === JSON.stringify(expectedDays.sort())).to.equals(true)
+  it('Valida dia semana fidelidade.', () => {
+    expect(tabela.getValorDiaria('fidelidade', 2)).to.equals(90)
+  })
+
+  it('Valida dia fds regular.', () => {
+    expect(tabela.getValorDiaria('regular', 6)).to.equals(80)
+  })
+
+  it('Valida dia fds fidelidade.', () => {
+    expect(tabela.getValorDiaria('fidelidade', 0)).to.equals(70)
   })
 })
 
 describe('Retorna o hotel mais barato.', () => {
-  const catalogo = new Catalogo()
-  
-  catalogo.add('Parque das flores', 3, 110, 80, 90, 80)
-  catalogo.add('Jardim Botânico', 4, 160, 110, 60, 50)
-  catalogo.add('Mar Atlântico', 5, 220, 100, 150, 40)
-  
-  describe('Entradas:', () => {
-    it('Regular: 16Mar2020(mon), 17Mar2020(tues), 18Mar2020(wed)', () => {
-      const input = 'Regular: 16Mar2020(mon), 17Mar2020(tues), 18Mar2020(wed)'
-      expect(catalogo.hotelMaisBarato(input)).to.equal('Parque das flores')
-    })
+  const hoteis = [
+    new Hotel('Parque das flores', 3, new TabelaPrecos(110, 80, 90, 80)),
+    new Hotel('Jardim Botânico', 4, new TabelaPrecos(160, 110, 60, 50)),
+    new Hotel('Mar Atlântico', 5, new TabelaPrecos(220, 100, 150, 40))
+  ]
+  const catalogo = new Catalogo(hoteis)
 
-    it('Regular: 20Mar2020(fri), 21Mar2020(sat), 22Mar2020(sun)', () => {
-      const input = 'Regular: 20Mar2020(fri), 21Mar2020(sat), 22Mar2020(sun)'
-      expect(catalogo.hotelMaisBarato(input)).to.equal('Jardim Botânico')
-    })
+  it('Deve retornar Parque das flores.', () => {
+    const input = new Input('Regular: 16Mar2020(mon), 17Mar2020(tues), 18Mar2020(wed)').format()
+    expect(catalogo.getHotelMaisBarato(input)).to.equals('Parque das flores')
+  })
 
-    it('Fidelidade: 26Mar2020(thur), 27Mar2020(fri), 28Mar2020(sat)', () => {
-      const input = 'Fidelidade: 26Mar2020(thur), 27Mar2020(fri), 28Mar2020(sat)'
-      expect(catalogo.hotelMaisBarato(input)).to.equal('Mar Atlântico')
-    })
+  it('Deve retornar Jardim Botânico.', () => {
+    const input = new Input('Regular: 20Mar2020(fri), 21Mar2020(sat), 22Mar2020(sun)').format()
+    expect(catalogo.getHotelMaisBarato(input)).to.equals('Jardim Botânico')
   })
 })
